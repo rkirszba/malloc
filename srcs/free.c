c603726837d3f82d1f4e6061dc17d8ed329c788b
Romain KIRSZBAUM <rkirszba@e2r4p10.42.fr>
2020-10-22 15:10:28 +0200

    first version of free

--- srcs/free.c
+++ srcs/free.c
@@ -3,16 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ezalos <ezalos@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 17:27:24 by ezalos            #+#    #+#             */
-/*   Updated: 2020/10/21 11:42:44 by ldevelle         ###   ########.fr       */
+/*   Updated: 2020/10/22 15:08:08 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "head.h"
 
-void		our_free(void *alloc)
+
+void	*get_alloc_header(void* alloc, t_zone *zone)
 {
-	free(alloc);
+	t_alloc_header	*alloc_cursor;
+
+	while (zone)
+	{
+		alloc_cursor = &zone->first_alloc_header;
+		while (TRUE)
+		{
+			if ((void*)alloc_cursor == alloc - sizeof(t_zone_header)
+				&& alloc_cursor->available == FALSE)
+				return alloc_cursor;
+			if (alloc_cursor->last == FALSE)
+				alloc_cursor += sizeof(t_zone_header) + alloc_cursor->size;
+			else
+				break ;	
+		}
+		zone = zone->header.next_zone;
+	}
+	return NULL;
+}
+
+void	free_alloc(t_alloc_header *alloc_header)
+{
+	alloc_header->available == TRUE;
+}
+
+void	our_free(void *alloc)
+{
+	t_malloc	*base;
+	void		*alloc_header;
+
+	base = *static_mem();
+	if (!(alloc_header = get_alloc_header(alloc, base->tiny_zone)))
+		if (!(alloc_header = get_alloc_header(alloc, base->small_zone)))
+			return;
+	free_alloc(alloc_header);
 }
