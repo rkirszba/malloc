fd88ffb676fa72c03b1c7c02e86e56c090b049cb
Romain Kirszbaum <rkirszba@student.42.fr>
2020-10-30 18:40:36 +0100

    ongoing debug

--- srcs/main_realloc_easy.c
+++ srcs/main_realloc_easy.c
@@ -6,7 +6,7 @@
 /*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 16:57:19 by rkirszba          #+#    #+#             */
-/*   Updated: 2020/10/30 17:36:22 by rkirszba         ###   ########.fr       */
+/*   Updated: 2020/10/30 18:11:54 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,22 @@ int main(void)
 	void *ptr;
 	void *ptr2;
 
-	ptr = our_malloc(213);
+	ptr = our_malloc(151);
 	printf("After first allocation, ptr = %p\n", ptr);
-	ptr = our_realloc(ptr, 240);
+	show_alloc_mem();
+	printf("\n\n");
+	ptr = our_realloc(ptr, 233);
 	printf("After bigger reallocation with space, ptr = %p\n", ptr);
-	ptr = our_realloc(ptr, 213);
+	show_alloc_mem();
+	printf("\n\n");
+	ptr = our_realloc(ptr, 151);
 	printf("After smaller reallocation, ptr = %p\n", ptr);
+	show_alloc_mem();
+	printf("\n\n");
 	ptr2 = our_malloc(213);
 	ptr = our_realloc(ptr, 213);
 	printf("After bigger reallocation without space, ptr = %p\n", ptr);
+	show_alloc_mem();
+	printf("\n\n");
 	return (0);
 }
--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 20:19:47 by rkirszba          #+#    #+#             */
-/*   Updated: 2020/10/30 17:35:14 by rkirszba         ###   ########.fr       */
+/*   Updated: 2020/10/30 18:16:07 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,7 @@ static void    *realloc_smaller(t_alloc_header *alloc_header, size_t size)
 	printf("In realloc smaller\n");
     if (SUCCESS == alloc_split(alloc_header, size))
 	{
+		printf("split worked !\n");
 		unavailable_remove(alloc_header);
 		alloc_set_unavailable(alloc_header);	/*
 												** only because it is set to
@@ -49,15 +50,19 @@ static void    *realloc_smaller(t_alloc_header *alloc_header, size_t size)
 												*/
 		unavailable_add(alloc_header);
         available_add(alloc_access_next(alloc_header));
+		defragment(alloc_access_next(alloc_header));
 	}
-    return (alloc_header + sizeof(t_alloc_header));
+    return ((void*)alloc_header + sizeof(t_alloc_header));
 }
 
 static void    *realloc_can(t_alloc_header *alloc_header, size_t size)
 {
 	printf("In realloc can\n");
     if (NULL == alloc_join_realloc(alloc_header, size))
+	{
+		printf("join failed\n");
         return (realloc_cant(alloc_header, size));
+	}
 	unavailable_remove(alloc_header);
 	alloc_set_unavailable(alloc_header);	/*
 											** only because it is set to
