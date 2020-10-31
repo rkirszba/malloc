458ee441444c354233678f7a5804137dff043cf9
Romain Kirszbaum <rkirszba@student.42.fr>
2020-10-31 12:36:18 +0100

    realloc passes tests

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 20:19:47 by rkirszba          #+#    #+#             */
-/*   Updated: 2020/10/30 18:16:07 by rkirszba         ###   ########.fr       */
+/*   Updated: 2020/10/31 12:33:34 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,41 +28,34 @@ static void    *realloc_cant(t_alloc_header *alloc_header, size_t size)
 {
     void            *new_ptr;
     
-	printf("In realloc cant\n");
     new_ptr = our_malloc(size);
     if (new_ptr)
         ft_memncpy(new_ptr, (void*)alloc_header + sizeof(t_alloc_header),
         alloc_header->size);
-    our_free((void*)alloc_header);
+    our_free((void*)alloc_header + sizeof(t_alloc_header));
     return new_ptr;
 }
 
 static void    *realloc_smaller(t_alloc_header *alloc_header, size_t size)
 {
-	printf("In realloc smaller\n");
     if (SUCCESS == alloc_split(alloc_header, size))
 	{
-		printf("split worked !\n");
 		unavailable_remove(alloc_header);
 		alloc_set_unavailable(alloc_header);	/*
 												** only because it is set to
 												** available in unavailable remove
 												*/
 		unavailable_add(alloc_header);
-        available_add(alloc_access_next(alloc_header));
 		defragment(alloc_access_next(alloc_header));
+        available_add(alloc_access_next(alloc_header));
 	}
     return ((void*)alloc_header + sizeof(t_alloc_header));
 }
 
 static void    *realloc_can(t_alloc_header *alloc_header, size_t size)
 {
-	printf("In realloc can\n");
     if (NULL == alloc_join_realloc(alloc_header, size))
-	{
-		printf("join failed\n");
         return (realloc_cant(alloc_header, size));
-	}
 	unavailable_remove(alloc_header);
 	alloc_set_unavailable(alloc_header);	/*
 											** only because it is set to
@@ -76,10 +69,10 @@ static void    *realloc_can(t_alloc_header *alloc_header, size_t size)
 void	*our_realloc(void *ptr, size_t size)
 {
     t_alloc_header  *alloc_header;
+	t_mem_type		*mem_type;
 
     if (ptr == NULL || size == 0)
     {
-		printf("Frist case \n");
         if (size == 0)
             our_free(ptr);
         return (our_malloc(size));
@@ -88,19 +81,15 @@ void	*our_realloc(void *ptr, size_t size)
 		return (NULL);
     alloc_header = ptr - sizeof(t_alloc_header);
     if (unavailable_exists((void*)alloc_header) == FALSE)
-	{
-		printf("Not in unavailable\n");
         return (NULL);
-	}
     size = secure_align_size(size);
-	printf("alloc header size   = %u\n", alloc_header->size);
-	printf("secure aligned size = %zu\n", size);
+	mem_type = mem_type_get_from_size(size);
     if (size <= alloc_header->size)
         return (realloc_smaller(alloc_header, size));
     else if (alloc_header->flags & HDR_TYPE_LARGE)
         return (realloc_cant(alloc_header, size));
-    else if (0 == (alloc_header->flags
-    & mem_type_get_from_size(size)->type))
+    else if (!mem_type || 0 == (alloc_header->flags
+    & mem_type->type))
         return (realloc_cant(alloc_header, size));
     else
         return (realloc_can(alloc_header, size));
--- test/main_realloc_easy.c
+++ test/main_realloc_easy.c
@@ -6,7 +6,7 @@
 /*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 16:57:19 by rkirszba          #+#    #+#             */
-/*   Updated: 2020/10/30 18:11:54 by rkirszba         ###   ########.fr       */
+/*   Updated: 2020/10/31 11:52:07 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,56 @@ int main(void)
 	void *ptr2;
 
 	ptr = our_malloc(151);
-	printf("After first allocation, ptr = %p\n", ptr);
+	printf("After first allocation\n");
+	printf("ptr = %p\n", ptr);
 	show_alloc_mem();
 	printf("\n\n");
+	// sleep(5);
+
 	ptr = our_realloc(ptr, 233);
-	printf("After bigger reallocation with space, ptr = %p\n", ptr);
+	printf("After bigger reallocation with space\n");
+	printf("ptr = %p\n", ptr);
 	show_alloc_mem();
 	printf("\n\n");
+	// sleep(5);
+
 	ptr = our_realloc(ptr, 151);
-	printf("After smaller reallocation, ptr = %p\n", ptr);
+	printf("After smaller reallocation\n");
+	printf("ptr = %p\n", ptr);
 	show_alloc_mem();
 	printf("\n\n");
-	ptr2 = our_malloc(213);
-	ptr = our_realloc(ptr, 213);
-	printf("After bigger reallocation without space, ptr = %p\n", ptr);
+	// sleep(5);
+
+	ptr2 = our_malloc(125);
+	printf("After new malloc reallocation\n");
+	printf("ptr2 = %p\n", ptr2);
 	show_alloc_mem();
 	printf("\n\n");
+	// sleep(5);
+
+	ptr = our_realloc(ptr, 273);
+	printf("After bigger reallocation without space\n");
+	printf("ptr = %p\n", ptr);
+	show_alloc_mem();
+	printf("\n\n");
+
+	our_free(ptr);
+	printf("After free of ptre\n");
+	show_alloc_mem();
+	printf("\n\n");
+
+/*
+	ptr = our_malloc(125);
+	show_alloc_mem();
+	printf("\n\n");
+
+	ptr2 = our_malloc(273);
+	show_alloc_mem();
+	printf("\n\n");
+
+	our_free(ptr);
+	show_alloc_mem();
+*/
+
 	return (0);
 }
