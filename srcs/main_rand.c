f8cd29c828de7af473787b362a4deb6b5ae05559
Romain Kirszbaum <rkirszba@student.42.fr>
2020-11-06 15:34:35 +0100

    main rand

--- srcs/main_rand.c
+++ srcs/main_rand.c
@@ -6,7 +6,7 @@
 /*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 15:01:26 by arobion           #+#    #+#             */
-/*   Updated: 2020/11/06 12:48:03 by rkirszba         ###   ########.fr       */
+/*   Updated: 2020/11/06 15:33:16 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@
 #include <time.h>
 #include <pthread.h>
 
-#define SIZE_TAB	5000
+#define SIZE_TAB	4000
 #define NB_TEST		1000000
 #define SIZE_ALLOC	16
 #define TINY		(RES_TINY * TINY_SIZE_MAX_FACTOR)
 #define SMALL		(RES_SMALL * SMALL_SIZE_MAX_FACTOR)
+#define THREAD_NB	8
+
+
+typedef struct s_thread_infos
+{
+	int			nb_tests;
+	pthread_t	*thread_tab;
+}				t_thread_infos;
+
 
 pthread_mutex_t g_lock;
 
-typedef	struct	s_thread_infos
-{
-	t_alloc_header	**tab;
-	int				nb_tests;
-}				t_thread_infos;
-
-size_t		size_tab[SIZE_TAB];
+size_t		size_tab[THREAD_NB][SIZE_TAB];
 
 size_t			get_size_alloc()
 {
@@ -84,7 +87,6 @@ int8_t		unit_test_malloc(t_alloc_header **tab)
 	test_read2(tab[r], secure_align_size(size_tab[r]));
 	our_free(tab[r]);
 	old = size_tab[r];
-	pthread_mutex_lock(&g_lock);
 	size_tab[r] = get_size_alloc();
 	printf(" (%6lu)", old);
 	old = (size_t)tab[r];
@@ -122,6 +124,7 @@ int8_t		unit_test(t_alloc_header **tab)
 	int8_t	retval;
 
 	type = rand() % 100;
+	/*
 	if (type < 35)
 	{
 		printf("realloc: ");
@@ -129,9 +132,10 @@ int8_t		unit_test(t_alloc_header **tab)
 	}
 	else
 	{
+		*/
 		printf("malloc:  ");
 		retval = unit_test_malloc(tab);
-	}
+	// }
 	type = rand() % 100;
 	if (type > 100)
 		our_free((void*)(((size_t)rand() << 32) + (size_t)rand()));
@@ -139,7 +143,21 @@ int8_t		unit_test(t_alloc_header **tab)
 	return (retval);
 }
 
-t_alloc_header		**init(void)
+size_t				get_thread_index(pthread_t current_thread, pthread_t *thread_tab)
+{
+	size_t	index;
+
+	index = 0;
+	while (index < THREAD_NB)
+	{
+		if (thread_tab[index] == current_thread)
+			return index;
+		index++;
+	}
+	return (0);
+}
+
+t_alloc_header		**init(pthread_t *thread_tab)
 {
 	t_alloc_header		**tab;
 	int					i;
@@ -152,8 +170,8 @@ t_alloc_header		**init(void)
 	{
 		if (!(tab[i] = our_malloc(SIZE_ALLOC)))
 			return (NULL);
-		size_tab[i] = SIZE_ALLOC;
-		test_write2(tab[i], secure_align_size(size_tab[i]));
+		size_tab[get_thread_index(pthread_self(), thread_tab)][i] = SIZE_ALLOC;
+		// test_write2(tab[i], secure_align_size(size_tab[i]));
 		i++;
 	}
 	return (tab);
@@ -171,23 +189,24 @@ void		finish(t_alloc_header **tab)
 	our_free(tab);
 }
 
-# define THREAD_NB	4
-
 
 void			*test_routine(void *thread_infos)
 {
-	int	i;
+	t_alloc_header	**tab;
+	int		i;
 
 	i = -1;
+	if (!(tab = init(((t_thread_infos*)thread_infos)->thread_tab)))
+		return (NULL);
 	while (++i < ((t_thread_infos*)thread_infos)->nb_tests)
 	{
 		printf("%d ", (int)pthread_self());
 		if (0 == i % 1000)
 			printf("%d\n", i);
-		if (ERROR == unit_test(((t_thread_infos*)thread_infos)->tab))
+		if (ERROR == unit_test(tab))
 			exit(1);
 	}
-	pthread_exit(NULL);
+	finish(tab);
 	return (NULL);
 }
 
@@ -196,32 +215,29 @@ void			*test_routine(void *thread_infos)
 
 int			main(int ac, char **av)
 {
-	t_thread_infos	thread_infos;
+	int				nb_tests;
 	int				i;
 	pthread_t 		thread_tab[THREAD_NB];
 
-	if (pthread_mutex_init(&g_lock, NULL) != 0)
-		printf("\n mutex init failed\n");
+	// if (pthread_mutex_init(&g_lock, NULL) != 0)
+	// 	printf("\n mutex init failed\n");
 	if (ac > 1)
-		thread_infos.nb_tests = atoi(av[1]);
+		nb_tests = atoi(av[1]);
 	else
-		thread_infos.nb_tests = NB_TEST;
+		nb_tests = NB_TEST;
 	if (ac > 2)
 		srand(atoi(av[2]));
 	else
 		srand(17);
-	if (!(thread_infos.tab = init()))
-		return (10);
 	write(1, "\n", 1);
 	i = -1;
 	while (++i < THREAD_NB)
-		if (pthread_create(&thread_tab[i], NULL, &test_routine, (void*)&thread_infos))
+		if (pthread_create(&thread_tab[i], NULL, &test_routine, (void*)(size_t)nb_tests))
 			return (1);
 	i = -1;
 	while (++i < THREAD_NB)
 		if (pthread_join(thread_tab[i], NULL))
 			return (1);
-	finish(thread_infos.tab);
-	pthread_mutex_destroy(&g_lock);
+	// pthread_mutex_destroy(&g_lock);
 	return (0);
 }
