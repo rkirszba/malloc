3e9aeac73401c3c674c9242f5e84192587955e08
ezalos <ezalos@gmail.com>
2020-11-16 12:16:21 +0100

    Script linux path independant

--- .tmp/script_linux.sh
+++ .tmp/script_linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # export DYLD_LIBRARY_PATH=. DYLD_INSERT_LIBRARIES="libft_malloc.so" DYLD_FORCE_FLAT_NAMESPACE=1
-export LD_PRELOAD="/home/ezalos/42/malloc/libft_malloc.so"
+export LD_PRELOAD="./libft_malloc.so"
 
 export HUGETLB_MORECORE=yes
 
