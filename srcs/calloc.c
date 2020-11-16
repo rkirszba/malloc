efb7a95ffb9e59ff8a0b20b3b253a2af94b7ddef
Romain Kirszbaum <rkirszba@student.42.fr>
2020-11-16 16:09:44 +0100

    calloc

--- srcs/calloc.c
+++ srcs/calloc.c
@@ -6,7 +6,7 @@
 /*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 17:16:30 by rkirszba          #+#    #+#             */
-/*   Updated: 2020/11/13 16:50:27 by ezalos           ###   ########.fr       */
+/*   Updated: 2020/11/16 16:07:34 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,24 @@
 void	*our_calloc(size_t count, size_t size)
 {
 	size_t	total_size;
+	size_t	i;
 	void	*ptr;
 
 	if (size == 0 || count == 0)
 		return (our_malloc(0));
 	if (static_mem()->is_init != TRUE)
 		malloc_init();
-	total_size = count * size;
+	total_size = 0;
+	i = 0;
+	while (i < count)
+	{
+		if (total_size + size < total_size)
+			return (NULL);
+		total_size += size;
+		i++;
+	}
+	if (secure_align_size(total_size) < total_size)
+		return (NULL);
 	ptr = our_malloc(total_size);
 	if (ptr)
 		ft_bzero(ptr, secure_align_size(total_size));
